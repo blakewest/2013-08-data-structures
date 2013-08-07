ca588f5913589f5059af629b8ab8abd1acfbcc2e
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:56:02 -0700

    got Stack passing for pseudo classical

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,20 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this._storage = {};
+  this._sizeCounter = 0;
+};
+
+Stack.prototype.push = function(value) {
+	this._storage[this._sizeCounter] = value;
+	this._sizeCounter++;
+};
+
+Stack.prototype.pop = function() {
+	this._sizeCounter && this._sizeCounter--;
+	var result = this._storage[this._sizeCounter];
+	delete this._storage[this._sizeCounter];
+	return result;
+};
+
+Stack.prototype.size = function() {
+	return this._sizeCounter;
 };
