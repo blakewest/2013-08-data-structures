2b78081e20920483d2861761b60de5a06c699b54
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:23:06 -0700

    Queue functional-shared passing

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,25 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  var newQueue = {};
+  newQueue.storage = {};
+  newQueue.idx = 0;
+  newQueue.tracker = 0;
+  _.extend(newQueue, makeQueue.methods);
+  return newQueue;
+};
+
+makeQueue.methods = {};
+
+makeQueue.methods.enqueue = function(value){
+  this.storage[this.idx] = value;
+  this.idx++;
+};
+
+makeQueue.methods.dequeue = function(){
+  var result = this.storage[this.tracker];
+  this.storage[this.tracker] && this.tracker++;
+  return result;
+};
+
+makeQueue.methods.size = function(){
+  return this.idx-this.tracker;
 };
