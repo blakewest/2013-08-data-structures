00e9c6ba9afbdb1a5f8df54822b2d0644098f275
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:35:34 -0700

    queue prototypal tests passing

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,24 @@
 var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var newQueue = Object.create(Queue.methods);
+  newQueue.storage = {};
+  newQueue.idx = 0;
+  newQueue.tracker = 0;
+  return newQueue;
 };
+
+Queue.methods = {
+	enqueue: function(value) {
+		this.storage[this.idx] = value;
+		this.idx++;
+	},
+	dequeue: function() {
+		var result = this.storage[this.tracker];
+		if(this.storage[this.tracker]) this.tracker++;
+		return result;
+	},
+	size: function() {
+		return this.idx - this.tracker;
+	}
+};
+
+
