ae15855532cb90232e3cfc24581cda070c3df107
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:35:53 -0700

    stack prototypal tests passing

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,24 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var newStack = Object.create(Stack.methods);
+  newStack.storage = {};
+  newStack.sizeCounter = 0;
+  return newStack;
+};
+
+Stack.methods = {};
+
+Stack.methods.push = function(value) {
+  this.storage[this.sizeCounter] = value ;
+  this.sizeCounter++;
+};
+
+Stack.methods.pop = function(){
+  this.sizeCounter && this.sizeCounter--;
+  var result = this.storage[this.sizeCounter];
+  delete this.storage[this.sizeCounter];
+  return result;
+};
+
+Stack.methods.size = function() {
+  return this.sizeCounter;
 };
