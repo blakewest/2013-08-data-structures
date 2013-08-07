f9eb601314f583c2f31295a38ba810af4b416ade
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:12:59 -0700

    Functional-shared stack passes the tests

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,25 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var newStack = {};
+  newStack.storage = {};
+  newStack.sizeCounter = 0;
+  _.extend(newStack, makeStack.methods);
+  return newStack;
+};
+
+makeStack.methods = {};
+
+makeStack.methods.push = function(value) {
+  this.storage[this.sizeCounter] = value ;
+  this.sizeCounter++;
+};
+
+makeStack.methods.pop = function(){
+  this.sizeCounter && this.sizeCounter--;
+  var result = this.storage[this.sizeCounter];
+  delete this.storage[this.sizeCounter];
+  return result;
+};
+
+makeStack.methods.size = function() {
+  return this.sizeCounter;
 };
