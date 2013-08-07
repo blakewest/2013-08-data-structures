3d766c8b3c72a643ab8bbe70e8ca79b0342a459a
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:42:56 -0700

    Added _ to private variables, fixed some indentation

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,24 +1,24 @@
 var Queue = function() {
   var newQueue = Object.create(Queue.methods);
-  newQueue.storage = {};
-  newQueue.idx = 0;
-  newQueue.tracker = 0;
+  newQueue._storage = {};
+  newQueue._idx = 0;
+  newQueue._tracker = 0;
   return newQueue;
 };
 
 Queue.methods = {
-	enqueue: function(value) {
-		this.storage[this.idx] = value;
-		this.idx++;
-	},
-	dequeue: function() {
-		var result = this.storage[this.tracker];
-		if(this.storage[this.tracker]) this.tracker++;
-		return result;
-	},
-	size: function() {
-		return this.idx - this.tracker;
-	}
+  enqueue: function(value) {
+    this._storage[this._idx] = value;
+    this._idx++;
+  },
+  dequeue: function() {
+    var result = this._storage[this._tracker];
+    if(this._storage[this._tracker]) this._tracker++;
+    return result;
+  },
+  size: function() {
+    return this._idx - this._tracker;
+  }
 };
 
 
