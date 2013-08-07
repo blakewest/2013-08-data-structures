2f65e4fe9832b5f87c5b7181d678c16e5bfd76b0
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:44:12 -0700

    Added _'s to private properties

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,24 +1,24 @@
 var Stack = function() {
   var newStack = Object.create(Stack.methods);
-  newStack.storage = {};
-  newStack.sizeCounter = 0;
+  newStack._storage = {};
+  newStack._sizeCounter = 0;
   return newStack;
 };
 
 Stack.methods = {};
 
 Stack.methods.push = function(value) {
-  this.storage[this.sizeCounter] = value ;
-  this.sizeCounter++;
+  this._storage[this._sizeCounter] = value ;
+  this._sizeCounter++;
 };
 
 Stack.methods.pop = function(){
-  this.sizeCounter && this.sizeCounter--;
-  var result = this.storage[this.sizeCounter];
-  delete this.storage[this.sizeCounter];
+  this._sizeCounter && this._sizeCounter--;
+  var result = this._storage[this._sizeCounter];
+  delete this._storage[this._sizeCounter];
   return result;
 };
 
 Stack.methods.size = function() {
-  return this.sizeCounter;
+  return this._sizeCounter;
 };
