bb3d3bc9754eb4369ed1a5ba49bb29409a5c4741
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:00:55 -0700

    Queue passes pseudo classical tests

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,20 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this._storage = {};
+  this._idx = 0;
+  this._tracker = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+	this._storage[this._idx] = value;
+	this._idx++;
+};
+
+Queue.prototype.dequeue = function() {
+	var result = this._storage[this._tracker];
+	if( this._storage[this._tracker] ) this._tracker++;
+	return result;
+};
+
+Queue.prototype.size = function() {
+	return this._idx - this._tracker;
 };
